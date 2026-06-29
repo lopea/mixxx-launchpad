57e05cde16447de0734a658b50f6d4e0a1b48a19
lopea <lopea5643@gmail.com>
2026-06-29 13:46:03 -0700

    Added launchpad pro support

---
+++ packages/launchpad-pro-mk1/src/index.ts
@@ -0,0 +1,60 @@
+import { convertControlDef, LaunchpadDevice, useDevice } from '@mixxx-launch/launchpad-common'
+import def from '../controls'
+import { MidiControlDef } from '@mixxx-launch/common/midi'
+import { sendSysexMsg } from '@mixxx-launch/mixxx'
+import { RGBColor } from '@mixxx-launch/common/color'
+import { Color } from '@mixxx-launch/launch-common'
+
+const colors = {
+  [Color.Black]: 0,
+  [Color.RedLow]: 7,
+  [Color.RedHi]: 5,
+  [Color.GreenLow]: 19,
+  [Color.GreenHi]: 17,
+  [Color.AmberLow]: 43,
+  [Color.AmberHi]: 41,
+  [Color.OrangeHi]: 84,
+  [Color.OrangeLow]: 61,
+  [Color.YellowHi]: 13,
+  [Color.YellowLow]: 15,
+}
+
+enum DeviceMode {
+  Note,
+  Drum,
+  Fader,
+  Programmer,
+}
+
+const selectMode = (mode: DeviceMode) => {
+  sendSysexMsg([240, 0, 32, 41, 2, 44, mode, 247])
+}
+
+class LaunchpadProMK1Device extends LaunchpadDevice {
+  supportsRGBColors: boolean
+  controls: { [key: string]: MidiControlDef }
+  colors: { [key in Color]: number }
+
+  constructor() {
+    super()
+    this.controls = Object.fromEntries(
+      Object.entries(def().controls).map(([k, v]) => [
+        k,
+        convertControlDef(k, v as [number, number]),
+      ]),
+    )
+    this.colors = colors
+    this.supportsRGBColors = true
+  }
+
+  override onMount() {
+    selectMode(DeviceMode.Programmer)
+    super.onMount()
+  }
+
+  sendRGBColor(control: MidiControlDef, color: RGBColor) {
+    sendSysexMsg([240, 0, 32, 41, 2, 11, control.midino, ...color.map((x) => ~~(x / 2)), 247])
+  }
+}
+
+export default useDevice(new LaunchpadProMK1Device())
